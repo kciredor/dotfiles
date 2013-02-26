da0fbd743c7413e9fbaec8d09ee5ed55dda08326
Roderick Schaefer <roderick@wehandle.it>
2013-02-26 15:14:41 +0100

    Reverted to mutt without offlineimap, improved config

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,7 @@ rm ~/.inputrc
 rm ~/.profile
 rm ~/.vimrc
 rm ~/.muttrc
-rm ~/.offlineimaprc
 rm ~/.mailcap
-rm ~/.msmtprc
 rm ~/.goobookrc
 
 echo "\n**** Deploying sweetness"
@@ -21,9 +19,7 @@ ln -s `pwd`/inputrc ~/.inputrc
 ln -s `pwd`/profile ~/.profile
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/muttrc ~/.muttrc
-ln -s `pwd`/offlineimaprc ~/.offlineimaprc
 ln -s `pwd`/mailcap ~/.mailcap
-ln -s `pwd`/msmtprc ~/.msmtprc
 ln -s `pwd`/goobookrc ~/.goobookrc
 
 mkdir -p ~/.vim/backup/
@@ -35,11 +31,6 @@ mkdir ~/.mutt/cache/bodies
 mkdir ~/.mutt/accounts
 touch ~/.mutt/certificates
 git clone https://github.com/altercation/mutt-colors-solarized ~/.mutt/mutt-colors-solarized
-ln -s `pwd`/mailrun.sh ~/.mutt/mailrun.sh
 cd ~/.mutt && wget http://filibusta.crema.unimi.it/~gufo/files/view_attachment.sh && chmod 700 view_attachment.sh
-
-mkdir ~/.offlineimap && chmod 700 ~/.offlineimap
-ln -s `pwd`/offlineimap-support.py ~/.offlineimap/offlineimap-support.py
-mkdir ~/.maildir && chmod 700 ~/.maildir
 
 echo "\n\n**** Done (now install bundles from within vim)\n\n"
