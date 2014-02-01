70f175229589ed892dff138c6c124cc49ea19129
Roderick Schaefer <roderick@wehandle.it>
2014-02-01 21:56:38 +0100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,87 @@
 #!/bin/sh
 
-# TODO: clean, deploy, deploy-bash, deploy-zsh, deploy-mutt-imap, deploy-mutt-offlineimap; add gpg
+dotfiles_zsh=(zpreztorc zlogin zlogout zpreztorc zprezto zprofile zshenv zshrc)
+dotfiles_bash=(inputrc bashrc profile)
+dotfiles_mutt=(goobookrc muttrc mutt mailcap mailrc gnupg/gpg-agentconf gnupg/gpgconf)
+dotfiles_muttofflineimap=(offlineimap offlineimaprc msmtprc)
+dotfiles_mac=(slate)
+dotfiles_other=(vimrc gitconfig gitignore)
+dotfiles_cleanup=(${dotfiles_zsh[*]} ${dotfiles_bash[*]} ${dotfiles_mutt[*]} ${dotfiles_muttofflineimap[*]} ${dotfiles_mac[*]} ${dotfiles_other[*]})
 
-echo "\n\n**** Removing current config files"
+echo "\nkciredor's dotfiles deploy\n\n** Have you cloned dotfiles recursively? If not: git submodule update --init --recursive before you deploy **\n"
 
-rm ~/.gitconfig
-rm ~/.gitignore
-rm ~/.inputrc
-rm ~/.bashrc
-rm ~/.vimrc
-rm ~/.goobookrc
-rm ~/.muttrc
-rm ~/.mutt
-# rm ~/.offlineimaprc
-# rm ~/.mailcap
-# rm ~/.mailrc
-# rm ~/.msmtprc
+read -p "Remove currently supported dotfiles from homefolder? [y/n] " -n 1 -r
 
-echo "\n**** Deploying sweetness"
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    for item in ${dotfiles_cleanup[*]}; do
+        rm -rf ~/.$item
+    done
 
-ln -s `pwd`/gitconfig ~/.gitconfig
-ln -s `pwd`/gitignore ~/.gitignore
-ln -s `pwd`/inputrc ~/.inputrc
-ln -s `pwd`/bashrc ~/.bashrc
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/muttrc ~/.muttrc
-ln -s `pwd`/goobookrc ~/.goobookrc
-ln -s `pwd`/mutt ~/.mutt
-ln -s `pwd`/mutt/imap_config `pwd`/mutt/specific_config
-# ln -s `pwd`/offlineimaprc ~/.offlineimaprc
-# ln -s `pwd`/mailcap ~/.mailcap
-# ln -s `pwd`/mailrc ~/.mailrc
-# ln -s `pwd`/msmtprc ~/.msmtprc
+    echo "Removed: ${dotfiles_cleanup[*]}"
+fi
 
-# mkdir ~/.maildir && chmod 700 ~/.maildir
+echo "\n"
+read -p "Use zsh? (otherwise installs bash dotfiles) [y/n] " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "\ninstall zsh\n"
+
+    for item in ${dotfiles_zsh[*]}; do
+        ln -s `pwd`/$item ~/.$item
+    done
+else
+    echo "\ninstall bash\n"
+
+    for item in ${dotfiles_bash[*]}; do
+        ln -s `pwd`/$item ~/.$item
+    done
+fi
+
+echo "\n"
+read -p "Install mail setup (includes mutt) [y/n] " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "\ninstall mutt\n"
+
+    mkdir -p ~/.gnupg/
+
+    for item in ${dotfiles_mutt[*]}; do
+        ln -s `pwd`/$item ~/.$item
+    done
+
+    echo "\n"
+    read -p "Use mutt with offlineimap? [y/n] " -n 1 -r
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "\ninstall mutt with offlineimap\n"
+
+        mkdir ~/.maildir && chmod 700 ~/.maildir
+
+        for item in ${dotfiles_muttofflineimap[*]}; do
+            ln -s `pwd`/$item ~/.$item
+        done
+    fi
+fi
+
+echo "\n"
+read -p "Install Mac OS specifics [y/n] " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "\ninstall mac dotfiles\n"
+
+    for item in ${dotfiles_mac[*]}; do
+        ln -s `pwd`/$item ~/.$item
+    done
+fi
+
+echo "\nInstalling remaining dotfiles"
+
+for item in ${dotfiles_other[*]}; do
+    ln -s `pwd`/$item ~/.$item
+done
 
 mkdir -p ~/.vim/backup/
 mkdir -p ~/.vim/bundle/
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s `pwd`/vundle ~/.vim/bundle/vundle
 
-echo "\n\n**** Done (now install bundles from within vim)\n\n"
+echo "\n\n**** Done (now install bundles from within vim using :BundleInstall)\n\n"
